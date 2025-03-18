42a9b63127716738490432c8bbd8396b11c673ae
hamehyo <hsham@pcninc.co.kr>
2025-03-18 09:34:51 +0900

    mobile 100vh issue modify2

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -64,11 +64,12 @@ $(function (){
     function setScreenSize() {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
+        console.log(vh);
     }
-    setScreenSize();
+    //setScreenSize();
 
-    $( window ).resize(function() {
-        setScreenSize();
+    $(window).resize(function() {
+        //setScreenSize();
     }); 
 
 });
