c86195cf1327ca662ee32632c082f35733c1f1cc
hamehyo <hsham@pcninc.co.kr>
2025-03-18 10:37:25 +0900

    script source modify

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -29,30 +29,27 @@ $(function (){
         preScrollTop = nextScrollTop;
     });
 
-
     /* SCROLL CHECK */
     const $counters = $(".scroll_on");
-    const exposurePercentage = 100; // 노출 비율(%)과 애니메이션 반복 여부(true/false)를 설정 /스크롤 했을 때 $counters 컨텐츠가 화면에 100% 노출되면 숫자가 올라감
-    const loop = true; // 애니메이션 반복 여부를 설정 / true로 설정할 경우, 요소가 화면에서 사라질 때 다시 숨겨짐
+    const exposurePercentage = 100;
+    const loop = true;
 
     $(window).on('scroll', function() { 
-        $counters.each(function() { // 각 "scroll_on" 클래스를 가진 요소에 대해 반복
+        $counters.each(function() {
             const $el = $(this);
-            const rect = $el[0].getBoundingClientRect();// 요소의 위치 정보를 가져옴
-            const winHeight = window.innerHeight; // 현재 브라우저 창의 높이
-            const contentHeight = rect.bottom - rect.top; // 요소의 높이
-            // 요소가 화면에 특정 비율만큼 노출될 때 처리함
+            const rect = $el[0].getBoundingClientRect();
+            const winHeight = window.innerHeight;
+            const contentHeight = rect.bottom - rect.top;
             if (rect.top <= winHeight - (contentHeight * exposurePercentage / 110) && rect.bottom >= (contentHeight * exposurePercentage / 110)) {
                 $el.addClass('active');
             }
-            // 요소가 화면에서 완전히 사라졌을 때 처리합
             if (loop && (rect.bottom <= 0 || rect.top >= window.innerHeight)) {
                 $el.removeClass('active');
             }
         });
     }).scroll();
 
-
+    /* MO -> AOS SCROLL ELEMENT OPTION REMOVE */
     function mo_aos_delay(){
         if( $(window).width() < 1024){
             $(".aspiration_box_inner ul li").removeAttr("data-aos-delay"); // team page
@@ -61,13 +58,14 @@ $(function (){
     }
     mo_aos_delay();
 
+    /* MO -> height 100vh */
     function setScreenSize() {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
-        console.log(vh);
     }
     setScreenSize();
 
+    /* RESIZE FUNCTION */
     $(window).resize(function() {
         setScreenSize();
     }); 
