d553d2e48657c2b20e5d2cc027df8b15575a15d7
hamehyo <hsham@pcninc.co.kr>
2025-03-18 09:28:24 +0900

    mobile 100vh issue modify

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -60,11 +60,17 @@ $(function (){
         }
     }
     mo_aos_delay();
+
+    function setScreenSize() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setScreenSize();
+
     $( window ).resize(function() {
-        //mo_aos_delay();
+        setScreenSize();
     }); 
 
 });
-
 
 
